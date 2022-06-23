44e4800778afbc76ec786dc702313ce1559b026d
skjaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-23 10:39:50 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,28 @@
+# b='mahindra'
+# print(b[:5]) # mahin
+
+# print(b[:7]) # mahindr
+
+# print(b[:3]) # mah
+
+# print(b[:8]) # mahindra
+
+# print(b[:1]) # m
+
+# print(b[2:5]) # hin
+
+# print(b[3:]) # indra
+
+# print(b[5:]) # dra
+
+# print(b[0:6]) # mahind
+
+
+a='my mobile is mi'
+# print(a[:10]) # my mobile
+
+# print(a[:12]) # my mobile is
+
+# print(a[:15]) # my mobile is mi
+
+# print(a[3:9]) # mobile
